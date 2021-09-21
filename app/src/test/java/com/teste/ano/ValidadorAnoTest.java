e6e50adda025395e48f2ea85d5ea81edbb1d5ea3
Matheus Amendoeira Ferraria <37910467+matheusaf@users.noreply.github.com>
2021-09-21 03:16:30 +0000

    adjusting app

--- app/src/test/java/com/teste/ano/ValidadorAnoTest.java
+++ app/src/test/java/com/teste/ano/ValidadorAnoTest.java
@@ -1,7 +1,7 @@
 package com.teste.ano;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class ValidadorAnoTest{
     private ValidadorAno _validadorAno;
